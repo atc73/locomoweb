dcae7d6386cc3f3140b65555a89b747d3f036ca5
Joris <joris.sellam@gmail.com>
2022-07-22 12:13:49 +0200

    initial

---
+++ components/header.tsx
@@ -0,0 +1,29 @@
+import headerStyles from './header.module.scss'
+import Link from "next/link"
+
+const header = () => {
+  return (
+    <main className={headerStyles.mainContainer}>
+      <div className={headerStyles.leftLogoDiv}>
+        <h1>Locomoweb</h1>
+      </div>
+      <div className={headerStyles.menuLargeScreen}>
+        <ul>
+          <li><Link href='#home'>Home</Link></li>
+          <li><Link href='#Pricing'>Pricing</Link></li>
+          <li><Link href='#Contact'>Contact</Link></li>
+        </ul>
+      </div>
+      <div className={headerStyles.menuSmallScreen}>
+        <div className={headerStyles.checkbox}></div>
+        <div className={headerStyles.hamburgerMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default header
